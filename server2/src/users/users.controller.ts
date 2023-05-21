01fe1c5cca2b14e4b882fda2002b0874819f2496
RomainC75 <rom.chenard@gmail.com>
2023-05-21 22:38:27 +0200

    go to 78

--- server2/src/users/users.controller.ts
+++ server2/src/users/users.controller.ts
@@ -1,21 +1,28 @@
-import { Body, Controller, Delete, Get , NotFoundException, Param, Patch, Post, Query, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get , NotFoundException, Param, Patch, Post, Query, ClassSerializerInterceptor, UseInterceptors, Session } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from 'src/reports/dtos/create-user.dto';
 import { UpdateUserDto } from 'src/reports/dtos/update-user.dto';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from 'src/reports/dtos/user.dto';
+import { AuthService } from './auth.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-    constructor(private usersService: UsersService){}
-    
+    constructor(private usersService: UsersService, private authService: AuthService){}
+
     @Post('/signup')
     createUser(@Body() body:CreateUserDto){
         console.log("body : ", body)
-        this.usersService.create(body.email, body.password)
+        return this.authService.signup(body.email, body.password)
     }
 
-    @Serialize(UserDto)
+    @Post('/signin')
+    signin(@Body() body:CreateUserDto){
+        return this.authService.signin(body.email, body.password)
+    }
+
+
     @Get('/:id')
     async findUser(@Param("id") id: string){
         console.log("handler running ...")
