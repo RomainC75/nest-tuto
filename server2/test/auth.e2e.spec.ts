91772e3d738f4643d24c9ad1811a72d090ae398d
RomainC75 <rom.chenard@gmail.com>
2023-05-29 20:22:45 +0200

    section 14 : done

--- server2/test/auth.e2e.spec.ts
+++ server2/test/auth.e2e.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { setupApp } from 'src/setup-app';
 
-describe('AppController (e2e)', () => {
+describe('Authentication System (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -12,22 +13,14 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // setupApp(app)
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    const newUser = {
-      "email":"rom4.chenard@gmail.com",
-      "password": "azerty"
-    }
+  it('handles a signup request', () => {
     return request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(newUser)
-      .expect(201)
-      .then((res)=>{
-        const {id, email} = res.body
-        expect(id).toBeDefined;
-        expect(email).toEqual(newUser.email)
-      })
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
   });
 });
